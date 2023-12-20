b7bf21ebdab6867c6f237be43ca9e899f4071ce3
dt <98478153+realcorvus@users.noreply.github.com>
2023-12-19 19:57:16 -0500

    limit infinity

--- lib/sobelow/finding.ex
+++ lib/sobelow/finding.ex
@@ -55,7 +55,7 @@ defmodule Sobelow.Finding do
     [finding.type, finding.vuln_source, filename, finding.vuln_line_no]
     |> IO.inspect()
     |> :erlang.term_to_binary()
-    |> IO.inspect()
+    |> IO.inspect(limit: :infinity)
     |> :erlang.md5()
     |> Base.encode16()
   end
